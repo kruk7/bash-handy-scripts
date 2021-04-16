f2eb8e3398a2156922f3a6418be6ddd7f212e2fd
Raven <kamil.raven7@gmail.com>
2021-04-17 00:48:56 +0200

    changed learn-progress script
    -added date to append string
    -fixed bug in creating data base file if it dosn' exists

--- learn-progress/Readme.md
+++ learn-progress/Readme.md
@@ -1,1 +1,3 @@
 Command Learn Progress Script (CLP Script)
+format to append:
+append.sh [command] ["description"]
--- learn-progress/append.sh
+++ learn-progress/append.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-data_file=data.b
 
-if [ ! -e "$data_file" ] ; then
-    touch "$data_file"
-    echo "[Command] - Description"
+full_path_script=$(realpath $0) #absolute path of this script with filename
+dir_path_script=$(dirname $full_path_script) #absolute path of this script without filename
+data_file=data.b # name of data base file
+database_path=$dir_path_script/$data_file #absolute path of data base file
+
+if [ ! -e "$database_path" ] ; then # create data base file i not exists
+    touch "$database_path"
+    echo "It's created $database_path data file"
 fi
 
-line_to_append="[$1] - $2"
+line_to_append="[$1]- $(date +%d-%m-%Y) - $2"
 
-echo "$line_to_append" >> "$data_file"
+echo "$line_to_append" >> "$database_path"
 
-cat "$data_file"
+echo "[Command] - Date - Description"
+cat "$database_path"
 
-a=($(wc $data_file))
+a=($(wc $database_path))
 lines=${a[0]}
 
 echo "It's appended $lines of lines"
