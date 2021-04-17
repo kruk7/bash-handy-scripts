c3756cb71bd04aa8bfd89f5f319174115fd79738
Raven <kamil.raven7@gmail.com>
2021-04-17 23:50:11 +0200

    changed learn-progress script
    added comments to code

--- learn-progress/append.sh
+++ learn-progress/append.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+# vars
+full_path_script=$(realpath $0)               # absolute path of this script with filename
+dir_path_script=$(dirname $full_path_script)  # absolute path of this script without filename
+data_file=data.b                              # name of data base file
+database_path=$dir_path_script/$data_file     # absolute path of data base file
 
-full_path_script=$(realpath $0) #absolute path of this script with filename
-dir_path_script=$(dirname $full_path_script) #absolute path of this script without filename
-data_file=data.b # name of data base file
-database_path=$dir_path_script/$data_file #absolute path of data base file
-
-if [ ! -e "$database_path" ] ; then # create data base file i not exists
+# creating data base file if not exists
+if [ ! -e "$database_path" ] ; then
     touch "$database_path"
     echo "It's created $database_path data file"
 fi
 
-line_to_append="[$1]- $(date +%d-%m-%Y) - $2"
-
+# appending string literal to data base file
+line_to_append="[$1] - $(date +%d-%m-%Y) - $2"
 echo "$line_to_append" >> "$database_path"
 
-echo "[Command] - Date - Description"
-cat "$database_path"
-
+# counting lines in data base file
 a=($(wc $database_path))
 lines=${a[0]}
 
+# printing result
+echo "[Command] - Date - Description"
+cat "$database_path"
 echo "It's appended $lines of lines"
